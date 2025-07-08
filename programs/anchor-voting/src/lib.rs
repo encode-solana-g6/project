b257b627c1908a1316d2bbc640cf40a6c94fe346
Nicolas M <marshall.nicolas@gmail.com>
2025-07-08 22:05:12 +0100

    add connection to wallets/burner

--- programs/anchor-voting/src/lib.rs
+++ programs/anchor-voting/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
-declare_id!("4wRYvRWymeFK9vpCDV2Z1fAE6yKyzM6epS6vSv1D6UPB");
+declare_id!("6HCBijJ5ir1ZCqZhNtFJ1DyA57mnjBemqe31t2vHgnYr");
 
 #[program]
-pub mod anchor_init {
+pub mod anchor_voting {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
