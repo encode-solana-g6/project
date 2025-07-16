e7f1351cb4f2a57617c99bc788606e44fdb1e436
Nicolas M <marshall.nicolas@gmail.com>
2025-07-16 23:17:27 +0100

    fix vite polyfills

--- web-app/astro.config.mjs
+++ web-app/astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-// import { Buffer } from "buffer";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
+    plugins: [nodePolyfills()],
     define: {
       "process.env.ANCHOR_BROWSER": "true",
-      Buffer: { Buffer: Buffer },
     },
   },
 });
